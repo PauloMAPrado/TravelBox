6e46a8211203bb847fec1a7e370500f241956c60
Paulo Prado <90202939+PauloMAPrado@users.noreply.github.com>
2025-11-05 20:04:45 -0300

    Refactor Home widget to remove user details
    Removed user-related information and UI elements from the Home widget.

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -6,16 +6,13 @@ import 'package:travelbox/views/modules/footbar.dart';
 import 'package:travelbox/views/modules/header.dart';
 
 class Home extends StatefulWidget {
-  final Map<String, dynamic> user;
-  
-  const Home({super.key, required this.user});
+  const Home({super.key});
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,64 +38,7 @@ class _HomeState extends State<Home> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    SizedBox(height: 40.0),
-                    
-                    // Saudação personalizada
-                    Text(
-                      'Olá, ${widget.user['name']}!',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.poppins(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF333333),
-                      ),
-                    ),
-                    
-                    SizedBox(height: 10.0),
-                    
-                    Text(
-                      'Bem-vindo ao CofreTrip',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.poppins(
-                        fontSize: 16.0,
-                        color: Colors.grey[600],
-                      ),
-                    ),
-                    
-                    SizedBox(height: 40.0),
-                    
-                    // Card de informações do usuário
-                    Card(
-                      elevation: 4,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Sua conta:',
-                              style: GoogleFonts.poppins(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xFF1E90FF),
-                              ),
-                            ),
-                            SizedBox(height: 12.0),
-                            _buildUserInfo('Email', widget.user['email']),
-                            _buildUserInfo('CPF', _formatCPF(widget.user['cpf'])),
-                            _buildUserInfo('Telefone', _formatPhone(widget.user['phone'])),
-                            _buildUserInfo('Membro desde', 
-                              _formatDate(widget.user['createdAt'])),
-                          ],
-                        ),
-                      ),
-                    ),
-                    
-                    SizedBox(height: 40.0),
-                    
+                    SizedBox(height: 250.0),
                     Text(
                       'Aparentemente você não tem um cofre criado...',
                       textAlign: TextAlign.center,
@@ -107,9 +47,7 @@ class _HomeState extends State<Home> {
                         color: Color(0xFF333333),
                       ),
                     ),
-                    
                     SizedBox(height: 40.0),
-                    
                     ElevatedButton(
                       onPressed: () {
                         Navigator.push(
@@ -133,9 +71,7 @@ class _HomeState extends State<Home> {
                         ),
                       ),
                     ),
-                    
                     SizedBox(height: 16.0),
-                    
                     ElevatedButton(
                       onPressed: () {
                         Navigator.push(
@@ -159,7 +95,6 @@ class _HomeState extends State<Home> {
                         ),
                       ),
                     ),
-                    
                     SizedBox(height: 20.0),
                     Spacer(),
                   ],
@@ -173,60 +108,5 @@ class _HomeState extends State<Home> {
         ],
       ),
     );
-  }
-
-  // Widget para exibir informações do usuário
-  Widget _buildUserInfo(String label, String value) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 6.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '$label: ',
-            style: GoogleFonts.poppins(
-              fontWeight: FontWeight.w600,
-              fontSize: 14.0,
-            ),
-          ),
-          Expanded(
-            child: Text(
-              value,
-              style: GoogleFonts.poppins(
-                fontSize: 14.0,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  // Formatar CPF
-  String _formatCPF(String cpf) {
-    if (cpf.length == 11) {
-      return '${cpf.substring(0, 3)}.${cpf.substring(3, 6)}.${cpf.substring(6, 9)}-${cpf.substring(9)}';
-    }
-    return cpf;
-  }
-
-  // Formatar telefone
-  String _formatPhone(String phone) {
-    if (phone.length == 11) {
-      return '(${phone.substring(0, 2)}) ${phone.substring(2, 7)}-${phone.substring(7)}';
-    } else if (phone.length == 10) {
-      return '(${phone.substring(0, 2)}) ${phone.substring(2, 6)}-${phone.substring(6)}';
-    }
-    return phone;
-  }
-
-  // Formatar data
-  String _formatDate(String dateString) {
-    try {
-      final date = DateTime.parse(dateString);
-      return '${date.day}/${date.month}/${date.year}';
-    } catch (e) {
-      return dateString.split('T')[0];
-    }
   }
 }
